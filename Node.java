9ce3b0a643f11bac4974ee3566b9f0132898eb1a
josulopez1 <jlopezvelasquez507@gmail.com>
2026-04-28 19:08:47 -0600

    Create generic Node class for binary tree
    Added a generic Node class for binary tree implementation.

--- Node.java
+++ Node.java
@@ -1,1 +1,12 @@
+// Nodo del árbol binario
+class Node<E> {
+    E data;
+    Node<E> left;
+    Node<E> right;
 
+    public Node(E data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
