3e8b4385a8f4b6a6f1ddbcc6ae4081d1f80e3d1d
josulopez1 <jlopezvelasquez507@gmail.com>
2026-04-28 19:08:33 -0600

    Implement Association class for English-Spanish pairs

--- Association.java
+++ Association.java
@@ -1,1 +1,31 @@
 
+// Representa una asociación (english -> spanish)
+public class Association<K extends Comparable<K>, V> implements Comparable<Association<K, V>> {
+
+    private K key;     // palabra en inglés
+    private V value;   // traducción en español
+
+    public Association(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    // IMPORTANTE: esto permite que el BST ordene por la palabra en inglés
+    @Override
+    public int compareTo(Association<K, V> other) {
+        return this.key.compareTo(other.key);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+}
